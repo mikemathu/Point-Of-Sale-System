c4862531246eef45545e6195abf35a152785ae5d
Michael Mathu <mikemathu.dev@gmail.com>
2023-12-11 22:59:16 +0300

    Add inventory authorization

---
+++ PointOfSaleSystem.Web/Authorization/Inventory/CanManageInventoryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using PointOfSaleSystem.Service.Services.Security;
+
+namespace PointOfSaleSystem.Web.Authorization.Inventory
+{
+    public class CanManageInventory : IAuthorizationRequirement { }
+    public class CanManageInventoryHandler : AuthorizationHandler<CanManageInventory>
+    {
+        private readonly RoleService _privilegeService;
+        public CanManageInventoryHandler(RoleService roleService)
+        {
+            _privilegeService = roleService;
+        }
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CanManageInventory requirement)
+        {
+            IEnumerable<string> userPrivileges = _privilegeService.GetUserPrivileges();
+
+            if (userPrivileges.Contains("Can Manage Inventory"))
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ PointOfSaleSystem.Web/Authorization/Inventory/CanManageUnitOfMeasuresHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using PointOfSaleSystem.Service.Services.Security;
+
+namespace PointOfSaleSystem.Web.Authorization.Inventory
+{
+    public class CanManageUnitOfMeasures : IAuthorizationRequirement { }
+    public class CanManageUnitOfMeasuresHandler : AuthorizationHandler<CanManageUnitOfMeasures>
+    {
+        private readonly RoleService _privilegeService;
+        public CanManageUnitOfMeasuresHandler(RoleService roleService)
+        {
+            _privilegeService = roleService;
+        }
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CanManageUnitOfMeasures requirement)
+        {
+            IEnumerable<string> userPrivileges = _privilegeService.GetUserPrivileges();
+
+            if (userPrivileges.Contains("Can Manage Unit of Measures"))
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
