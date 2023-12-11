9e0179ec3a7ee164ccb5bccdfc1aa9fad1d42db0
Michael Mathu <mikemathu.dev@gmail.com>
2023-12-11 20:26:54 +0300

    Add sales interfaces

---
+++ PointOfSaleSystem.Service/Interfaces/Sales/IOrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using PointOfSaleSystem.Data.Inventory;
+
+namespace PointOfSaleSystem.Service.Interfaces.Sales
+{
+    public interface IOrderRepository
+    {
+        //Order
+        Task<bool> CreatePosOrderAsync();
+        Task<bool> AddPosItemQuantityAsync(CustomerOrder customerOrder, int quantity, double subTotal, double unitPrice);
+        Task<bool> AddPosItemToOrderAsync(CustomerOrder customerOrder, double unitPrice);
+        Task<bool> RemoveOrderAsync(int customerOrderID);
+        Task<IEnumerable<CustomerOrder>> GetUserPendingOrdersAsync();
+        Task<bool> DoesOrderExist(int customerOrderID);
+        Task<bool> IsOrderPaid(int customerOrderID);
+        Task<(int Quantity, double SubTotal)> IsItemAvailable(CustomerOrder customerOrder);
+
+        //Item
+        Task<IEnumerable<Item>> GetAllItemsOnPOSAsync();
+        Task<IEnumerable<ItemCategory>> GetAllItemCategoriesAsync();
+        Task<IEnumerable<OrderedItem>> GetPosOrderItemsAsync(int customerOrderID);
+        Task<double> GetItemUnitPriceAsync(int itemID);
+        Task<int> CheckQuantity(OrderedItem orderItem);
+        Task<IEnumerable<OrderedItem>> GetQuantity(int customerOrderID);
+        Task<bool> UpdatePosItemQuantity(OrderedItem orderItem, double unitPrice);
+        Task<bool> RemoveItem(OrderedItem orderItem);
+        Task<bool> DoesItemExist(int itemID);
+    }
+}
---
+++ PointOfSaleSystem.Service/Interfaces/Sales/IPaymentRepository.cs
@@ -0,0 +1,12 @@
+﻿using PointOfSaleSystem.Data.Sales;
+
+namespace PointOfSaleSystem.Service.Interfaces.Sales
+{
+    public interface IPaymentRepository
+    {
+        //Payment
+        Task<IEnumerable<PaymentMethod>> GetAllPaymentModesAsync();
+        Task<bool> ReceivePosPaymentsAsync(Payment payment, IEnumerable<OrderedItem> orderItemsProductQuantity, int userID, int fiscalPeriodID);
+        Task<double?> CalculateTotalOrderAmount(int customerOrderID);
+    }
+}
