4198f28bc24767112e9fe308c4dd5b3b99d2ce80
Michael Mathu <mikemathu.dev@gmail.com>
2023-12-11 23:03:03 +0300

    Add inventory controller

---
+++ PointOfSaleSystem.Web/Controllers/InventoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PointOfSaleSystem.Web.Controllers
+{
+    [Route("Inventory")]
+    [Authorize]
+    public class InventoryController : Controller
+    {
+        [Route("Inventory")]
+        public IActionResult Inventory()
+        {
+            return View();
+        }
+        [Route("StockTake")]
+        public IActionResult StockTake()
+        {
+            return View();
+        }
+        [Route("UnitOfMeasure")]
+        public IActionResult UnitOfMeasure()
+        {
+            return View();
+        }
+    }
+}
