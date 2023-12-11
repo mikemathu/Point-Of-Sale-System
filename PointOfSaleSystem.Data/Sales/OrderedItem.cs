175dddacaeaa1675dd65e57534eace217394b7dc
Michael Mathu <mikemathu.dev@gmail.com>
2023-12-11 20:07:23 +0300

    Add sales entity classes

---
+++ PointOfSaleSystem.Data/Sales/OrderedItem.cs
@@ -0,0 +1,13 @@
+﻿namespace PointOfSaleSystem.Data.Sales
+{
+    public class OrderedItem
+    {
+        public double SubTotal { get; set; }
+        public int Quantity { get; set; }
+        public int ItemID { get; set; }
+        public int CustomerOrderID { get; set; }
+        public string ItemName { get; set; }
+        public double UnitPrice { get; set; }
+        public string UnitOfMeasureName { get; set; }
+    }
+}
---
+++ PointOfSaleSystem.Data/Sales/Payment.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PointOfSaleSystem.Data.Sales
+{
+    public class Payment
+    {
+        public int CustomerOrderID { get; set; }
+        public PosPaymentItems[] PosPaymentItems { get; set; }
+    }
+    public class PosPaymentItems
+    {
+        public double AmountDue { get; set; }
+        public double AmountTendered { get; set; }
+        public double ChangeAmount { get; set; }
+        public int PaymentMethodID { get; set; }
+    }
+
+    public class PaymentMethod
+    {
+        public int PaymentMethodID { get; set; }
+        public int IsDefault { get; set; }
+        public string PaymentMethodName { get; set; }
+
+        [ForeignKey("SubAccountID")]
+        public int SubAccountID { get; set; }
+    }
+}
