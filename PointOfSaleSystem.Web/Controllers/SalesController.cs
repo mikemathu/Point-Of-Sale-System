a16dab8716f05082d03fcd9cd61979cd3c365956
Michael Mathu <mikemathu.dev@gmail.com>
2023-12-11 23:03:17 +0300

    Add sales controller

---
+++ PointOfSaleSystem.Web/Controllers/SalesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PointOfSaleSystem.Web.Controllers
+{
+    [Route("Sales")]
+    [Authorize]
+    public class SalesController : Controller
+    {
+        [Route("PointOfSale")]
+        public IActionResult PointOfSale()
+        {
+            return View();
+        }
+    }
+}
