b913ec2fd42fa800c90d28fd73e150fe072b3d4f
Michael Mathu <mikemathu.dev@gmail.com>
2023-12-11 23:02:45 +0300

    Add accounts controller

---
+++ PointOfSaleSystem.Web/Controllers/AccountsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PointOfSaleSystem.Web.Controllers
+{
+    [Route("Accounts")]
+    [Authorize]
+    public class AccountsController : Controller
+    {
+        [Route("GeneralLedgerAccounts")]
+        [Route("/")]
+        public IActionResult GeneralLedgerAccounts()
+        {
+            return View();
+        }
+
+        [Route("FiscalPeriods")]
+        public IActionResult FiscalPeriods()
+        {
+            return View();
+        }
+
+        [Route("ActivateSubAccount")]
+        public IActionResult ActivateSubAccount()
+        {
+            return View();
+        }
+        [Route("DeactivateSubAccount")]
+        public IActionResult DeactivateSubAccount()
+        {
+            return View();
+        }
+
+        [Route("JournalVouchers")]
+        public IActionResult JournalVouchers()
+        {
+            return View();
+        }
+        [Route("GetAllAccountsAsync")]
+        public IActionResult GetAllAccountsAsync()
+        {
+            return View();
+        }
+        [Route("Taxes")]
+        public IActionResult Taxes()
+        {
+            return View();
+        }
+    }
+}
