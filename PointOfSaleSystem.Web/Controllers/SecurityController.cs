8a8fb201e4a7690b0162636a9e6f8f5378adac98
Michael Mathu <mikemathu.dev@gmail.com>
2023-12-11 23:03:36 +0300

    Add security controller

---
+++ PointOfSaleSystem.Web/Controllers/SecurityController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PointOfSaleSystem.Web.Controllers
+{
+
+    [Route("Security")]
+    [Authorize]
+    public class SecurityController : Controller
+    {
+        [Route("Register")]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [Route("Login")]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("Roles")]
+        public IActionResult Roles()
+        {
+            return View();
+        }
+
+        [Route("Privileges")]
+        public IActionResult Privileges()
+        {
+            return View();
+        }
+        [Route("SystemUsers")]
+        public IActionResult SystemUsers()
+        {
+            return View();
+        }
+    }
+}
