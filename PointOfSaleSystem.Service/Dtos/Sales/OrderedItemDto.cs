9ac77cebdfa6368c191f1c04a9ada2e65a690f31
Michael Mathu <mikemathu.dev@gmail.com>
2023-12-11 20:16:42 +0300

    Add sales dtos

---
+++ PointOfSaleSystem.Service/Dtos/Sales/OrderedItemDto.cs
@@ -0,0 +1,13 @@
+﻿namespace PointOfSaleSystem.Service.Dtos.Sales
+{
+    public class OrderedItemDto
+    {
+        public double SubTotal { get; set; }
+        public int Quantity { get; set; }
+        public int ItemID { get; set; }
+        public int CustomerOrderID { get; set; }
+        public string ItemName { get; set; }
+        public double UnitPrice { get; set; }
+        public string UnitOfMeasureName { get; set; }
+    }
+}
---
+++ PointOfSaleSystem.Service/Dtos/Sales/PaymentDto.cs
@@ -0,0 +1,18 @@
+﻿using Point_Of_Slae.Models.Sales;
+
+namespace PointOfSaleSystem.Service.Dtos.Sales
+{
+    public class PaymentDto
+    {
+        public int CustomerOrderID { get; set; }
+        public PosPaymentItems[] PosPaymentItems { get; set; }
+    }
+    public class PaymentMethodDto
+    {
+        public int PaymentMethodID { get; set; }
+        public int IsDefault { get; set; }
+        public string PaymentMethodName { get; set; }
+        public int SubAccountID { get; set; }
+        public int CanBeReceived { get; set; } = 1; //TODO: Get this from DB instead of hard coding it
+    }
+}
