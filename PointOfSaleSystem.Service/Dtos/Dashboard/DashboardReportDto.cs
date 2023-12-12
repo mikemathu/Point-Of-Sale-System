41fbc66c04f3247b50d20fb3301bb2a43fc5dded
Michael Mathu <mikemathu.dev@gmail.com>
2023-12-12 17:21:33 +0300

    Add dashboard dto

---
+++ PointOfSaleSystem.Service/Dtos/Dashboard/DashboardReportDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PointOfSaleSystem.Service.Dtos.Dashboard
+{
+    public class DashboardReportDto
+    {
+        public int ReportID { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
